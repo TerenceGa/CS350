dcc65821b91d16cedcdf73af68a8ea2899ec8b74
unknown <tg6709032@gmail.com>
2024-11-01 09:05:36 -0400

    6

--- hw6/imglib.c
+++ hw6/imglib.c
@@ -585,8 +585,8 @@ uint8_t sendImage(struct image* img, int sockfd) {
     char magic[3] = {'I', 'M', 'G'};
     size_t to_send = img->width * img->height * sizeof(uint32_t);
     char * bufptr = (char *)(img->pixels);
-	printf("Sending width: %u (network order: %u)\n", img->width, width_net);
-	printf("Sending height: %u (network order: %u)\n", img->height, height_net);
+	printf("Sending width: %u (network order: %u)\n", img->width);
+	printf("Sending height: %u (network order: %u)\n", img->height);
 
     /* Send the magic bytes */
     if (send(sockfd, magic, 3, 0) != 3) {
@@ -644,12 +644,12 @@ struct image * recvImage(int sockfd) {
 	    recv(sockfd, &(height), sizeof(uint32_t), 0) != sizeof(uint32_t)) {
 		return NULL;
 	}
-	printf("Received width_net: %u\n", img->width, width_net);
+
 	printf("Converted width: %u\n", width);
 
-	printf("Received height_net: %u\n", img->height, height_net);
+
 	printf("Converted height: %u\n", height);
-	
+
 	printf("Received pixel[%zu]: %u (host order: %u)\n", i, pixel_net, img->pixels[i]);
 
 	/* Create a new image to fill up */
